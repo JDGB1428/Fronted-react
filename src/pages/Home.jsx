ba6803b229050e64ba46bd3e6d065d88b86d6e16
Juan David <juangalindo28@hotmail.com>
2022-06-18 17:03:36 -0500

    paginas de la web

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import Section from '../components/Section';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Section/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
